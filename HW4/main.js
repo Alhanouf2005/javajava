e9eafd7e5008d31fa6158fa05cfd90394eeb6523
Alhanouf <hhsj200512@gmail.com>
2023-08-10 15:49:58 +0300

    add

---
+++ HW4/main.js
@@ -0,0 +1,36 @@
+ // التمرين الأول 
+// //// //// num to str
+ let Num = Number(42);
+ const $Num = String(Num);
+ console.log(`${typeof $Num}:${$Num}`);
+ console.log(`${typeof  $Num}: ${$Num}`);
+
+ /////////str to num//
+ let str="25";
+const strTOnum=Number(str);
+console.log(`${typeof str} ${str}`);
+console.log(`${typeof strTOnum} ${strTOnum}`);
+
+
+/////////////
+//التمرين 2 ///
+let str1="10";
+let str1Tonum=Number(str1);
+let math= (str1)-5;
+let math2= (str)+6;
+console.log(`${typeof str1Tonum}:${str1Tonum}`);
+
+/////3//
+
+
+
+
+////4///
+
+
+
+
+
+/////5///
+
+ 
