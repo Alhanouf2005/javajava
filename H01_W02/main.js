e16a6dfcf64b05230179eac7fefb2416693054cf
Alhanouf <hhsj200512@gmail.com>
2023-08-14 17:28:25 +0300

    W02_H01

---
+++ H01_W02/main.js
@@ -0,0 +1,11 @@
+//Array//
+//First//
+let Names=["Alhanouf","Laith","Saad","Aloraini"];
+console.log(Names[2]);
+
+//Second//
+Names.unshift("Ali");
+Names.push("Samah");
+Names.shift();
+Names.pop();
+
