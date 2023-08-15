7f2ef54a5ba845c9c0b565a3f6f46f67df8f4f3d
Alhanouf <hhsj200512@gmail.com>
2023-08-16 00:50:44 +0300

    A2010

---
+++ condition/main.js
@@ -0,0 +1,109 @@
+//let number = 2;
+//if (number > 0) {
+
+    // code log
+//}
+//else {
+
+    // code
+
+
+//}
+
+// code  after if
+
+//let name = "Alhanouf";
+//let age = 11;
+
+//if (age > 15 && name == "Alhanouf"){
+  //  console.log("welcome Back to javaScript");
+//}
+
+//let number = 100;
+//if (number > 90){
+   // console.log("yes 90");
+//}
+
+//let number = prompt("what is your Score?")
+ //if (number >= 101) {
+   // alert("لا توجد قيمه ")
+ //}
+//else if (number >= 100 ) {
+  // alert("A+");
+ //}
+ //else if (number >= 90) {
+  //  alert("A");
+ //}
+//else if (number >= 90) {
+// alert("B+");
+ //}
+ //else if (number >= 85) {
+  //  alert("B");
+
+ //}
+//else if (number >= 80) {
+ //   alert("C");
+// }
+
+//let speed= prompt("كم كانت سرعتك؟") ;
+//let speedlimit = 60;
+//let message = speed > speedlimit ? "أنت تقود بسرعة زائدة":"أنت تقود بأمان";
+//alert(message);
+////////////////
+//let monthNumber =6;
+//let monthName;
+//switch (monthNumber) {
+  //  case 1:
+       // monthName = "يناير";
+        //break;
+
+        //case 2:
+        //monthName = "فبراير";
+        //break;
+        //case 3:
+            //monthName ="مارس";
+            //break;
+            //case 4:
+                //monthName ="ابريل";
+                //break;
+                //case 5:
+                    //monthName ="مايو";
+                    //break;
+                    //case 6:
+                        //monthName ="يونيو";
+                        //break;
+                        //case 7:
+                            //monthName ="يوليو";
+                          //  break;
+                          //  case 8:
+                              //  monthName ="أغسطس";
+                            //    break;
+                          //      case 9:
+                        //            monthName ="سبتمبر";
+                      //              break;
+                    //                case 10:
+                  //                  monthName = "أكتوبر";
+                //                    break;
+              //                      case 11:
+            //                            monthName ="نوفمبر";
+          //                              break;
+        //                                case 12:
+      //                                      monthName = "ديسمبر";
+    //                                        break;
+  //                                   default:
+//                                        monthName = "شهر غير صالح";
+//                                        break;
+//}
+//console.log("رقم الشهر:" +monthNumber);
+//console.log("اسم الشهر :"+monthName);
+
+
+//let isLoggedIn = false;
+
+//if (!isLoggedIn) {
+   // console.log("يرجى تسجيل الدخول.");
+
+//}else{
+  //  console.log("مرحبا بعودتك !");
+//}
+
