95090c08581ac553cb566635a93915cc1c1b88d6
Alhanouf <hhsj200512@gmail.com>
2023-08-16 01:37:19 +0300

    2010

---
+++ condition HW/main.js
@@ -0,0 +1,18 @@
+////حل الواجب///
+///1///
+ //let name =prompt("الاسم :")
+ //let age =prompt("العمر :")
+
+ //if (age >= 18) {
+  //  alert("يا أهلا وسهلا" +name )
+ //}else{
+   // alert("ماعليه الجايات أكثر" + name) 
+ //}
+ ////2////
+ //let name_1 = prompt("enter your name")
+
+ //if (name_1 == "ahmed"){
+   // alert("The two names are identical")
+ //}else{
+   // alert("The two names do not identical")
+ //}
