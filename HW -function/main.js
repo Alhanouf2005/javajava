9b148cfdc4a017550ef0f188e47e86964b96a048
Alhanouf <hhsj200512@gmail.com>
2023-08-21 05:15:48 +0300

    Fun

---
+++ HW -function/main.js
@@ -0,0 +1,17 @@
+////////Function HomeWork///////
+/////////////One/////////////
+////let javascript_Boot_camp = (teach) => {
+
+  ////  alert(" This javaScript BootCamp with Teacher1 OK?");
+   /// alert(" This javaScript BootCamp with Teacher2 OK?");
+//}
+/////////////Two///////////
+//let average = (num1, num2) => {
+  //  let NumAll = num1 - num2;
+//}
+//alert(NumAll +2)
+
+/////int///
+//average(4, 8)
+////dou///
+//average(6.5, 8.5)
